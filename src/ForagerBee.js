46e7f7ad6747fee4ac98f9cb272acafcb1d678d4
hyomithetech <hyomithetech@gmail.com>
2019-11-03 20:29:27 -0800

    Complete 6ees6ees6ees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
+class ForagerBee extends Bee{
+  constructor() {
+    super()
+    this.age = 10;
+    this.job = `find pollen`
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage () {
+    this.treasureChest.push(`treasure`)
+  }
   // TODO..
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,19 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee{
+  constructor() {
+    super()
+    this.age = 40;
+    this.job = `gamble`;
+    this.canFly = false;
+    this.color = `grey`;
+
+  }
+
+  forage () {
+    return `I am too old, let me play cards instead`
+  }
+
+  gamble () {
+    this.treasureChest.push(`treasure`)
+  }
   // TODO..
 };
